25ef74ad84c8af8ba4d0b98d156bc3a9c60cab3e
santzk1ng <edmilsonjunin999@gmail.com>
2025-11-28 00:33:20 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,20 @@ TOKEN = "8563989647:AAFQXyZltk7T4-GugA7UY_pcP4RRDmnNCYA"
 
 bot = telebot.TeleBot(TOKEN)
 
+# Comando /start
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Fala irmão, o bot do santzk1ng tá ON!")
 
-@bot.message_handler(func=lambda m: True)
+# Receber FOTO
+@bot.message_handler(content_types=['photo'])
+def photo_handler(message):
+    bot.send_message(message.chat.id, "Recebi a imagem irmão!")
+
+# Responder qualquer TEXTO
+@bot.message_handler(func=lambda m: True, content_types=['text'])
 def echo(message):
     bot.send_message(message.chat.id, f"Recebi: {message.text}")
 
 bot.polling()
+
